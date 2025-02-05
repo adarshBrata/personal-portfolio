3688f1c7543d66c1e039dd9249a10c5ebd18b387
Adarsh Pal <adarshpal@Adarshs-MacBook-Pro.local>
2025-02-06 00:08:08 +0530

    update docker file

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,13 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), icon()]
+  integrations: [react(), tailwind(), icon()],
+  output: 'server',
+  adapter: node({
+    mode: "standalone"
+  })
 });
