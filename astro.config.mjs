133a713143ed3da3cec1d4d572481f7a63661865
adarshBrata <adarsh.brata@gmail.com>
2025-03-28 04:01:23 +0530

    fix: add amplify support for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,29 +2,18 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import icon from "astro-icon";
 import node from "@astrojs/node";
-import { fileURLToPath, URL } from 'url';
-
+import awsAmplify from 'astro-aws-amplify';
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), icon()],
   output: 'server',
-
-  adapter: node({
-    mode: "standalone"
-  }),
-
+  adapter: awsAmplify(),
   experimental: {
     svg: true,
   },
-
   vite: {
     plugins: [tailwindcss()],
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      }
-    }
   },
 });
