c1c2f93dc06111c568aea9e57029b3331e43d7f9
Adarsh Brata <31387483+adarshBrata@users.noreply.github.com>
2025-07-04 21:33:54 +0530

    Updated astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-import icon from "astro-icon";
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), icon(), tailwind()],
+  integrations: [react(), tailwind(), icon()],
   output: 'static',
   experimental: {
     svg: true,
