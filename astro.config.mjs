8105a5986a8669913197cf7284a3eb8c9a792b3f
Adarsh Brata <31387483+adarshBrata@users.noreply.github.com>
2025-07-04 21:23:18 +0530

    Updated package-lock.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import icon from "astro-icon";
-import tailwindcss from "@tailwindcss/vite";
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), icon()],
+  integrations: [react(), icon(), tailwind()],
   output: 'static',
   experimental: {
     svg: true,
-  },
-  vite: {
-    plugins: [tailwindcss()],
   },
 });
